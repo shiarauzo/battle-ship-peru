2e02a04cb33051eec15cda48d250b574215df463
shiara <shiara.arauzo@gmail.com>
2025-12-09 21:47:15 -0500

    add drizzle config and fix database schema

---
+++ drizzle.config.ts
@@ -0,0 +1,10 @@
+﻿import { defineConfig } from "drizzle-kit";
+
+export default defineConfig({
+  dialect: "postgresql",
+  out: "./drizzle",
+  schema: "./db/schema.ts",
+  dbCredentials: {
+    url: process.env.DATABASE_URL!,
+  },
+});
